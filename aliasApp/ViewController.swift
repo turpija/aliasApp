04035be141de64c89769a7e3069f031a121fda06
toorpia <turpija@gmail.com>
2018-04-15 18:37:07 +0200

    Initial Commit

---
+++ aliasApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aliasApp
+//
+//  Created by Ivan Kočiš on 15/04/2018.
+//  Copyright © 2018 toorpia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
